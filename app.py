cdd4409e4a58b7375fa87be83976a55c310106d9
Holmes629 <98832423+Holmes629@users.noreply.github.com>
2024-03-27 14:40:43 +0530

    web

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ cnn_model= keras.models.load_model(cnn_model_path)
 rnn_model= keras.models.load_model(rnn_model_path)
 app= Flask(__name__)
 
-@app.route('/')
+@app.route('/home')
 def home():
     print("working")
     return render_template('index.html')
