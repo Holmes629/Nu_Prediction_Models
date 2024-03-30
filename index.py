7c9badd040e8201a1ba28bf07632bb83d7a111e7
Holmes629 <98832423+Holmes629@users.noreply.github.com>
2024-03-30 19:54:08 +0530

    update

--- index.py
+++ index.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, jsonify
 import pickle
 import numpy as np
 import pandas as pd
-import keras
+from tensorflow import keras
 import os
 
 current_directory = os.path.dirname(os.path.abspath(__file__))
