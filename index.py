0aa111c5e6e16f96557fd21f2e85c09b5eb19eaa
Holmes629 <98832423+Holmes629@users.noreply.github.com>
2024-04-01 14:32:23 +0530

    up

--- index.py
+++ index.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, jsonify
 import pickle
 import numpy as np
 import pandas as pd
-# from tensorflow import keras
+from tensorflow import keras
 import os
 
 current_directory = os.path.dirname(os.path.abspath(__file__))
@@ -14,8 +14,8 @@ svm_path = os.path.join(current_directory, "Model_Packages", "1_SVM_Model_Packag
 log_lin_reg_path = os.path.join(current_directory, "Model_Packages", "2_Log_Linear_Regression_Model_Package.pkl")
 log_svm_path= os.path.join(current_directory, "Model_Packages", "2_Log_SVM_Model_Package.pkl")
 # models_path = os.path.join(current_directory, "Model_Packages", "2_Random_Forest_Model_Package.pkl")
-# fnn_model_path = os.path.join(current_directory, "Model_Packages", "FNN_Model_Package.h5")
-# cnn_model_path = os.path.join(current_directory, "Model_Packages", "CNN_Model_Package.h5")
+fnn_model_path = os.path.join(current_directory, "Model_Packages", "FNN_Model_Package.h5")
+cnn_model_path = os.path.join(current_directory, "Model_Packages", "CNN_Model_Package.h5")
 # rnn_model_path = os.path.join(current_directory, "Model_Packages", "RNN_Model_Package.h5")
 
 with open(lin_reg_path, 'rb') as f:
@@ -26,8 +26,8 @@ with open(log_lin_reg_path, 'rb') as f:
     log_lin_reg_model= pickle.load(f)
 with open(log_svm_path, 'rb') as f:
     log_svm_model= pickle.load(f)
-# fnn_model= keras.models.load_model(fnn_model_path)
-# cnn_model= keras.models.load_model(cnn_model_path)
+fnn_model= keras.models.load_model(fnn_model_path)
+cnn_model= keras.models.load_model(cnn_model_path)
 # rnn_model= keras.models.load_model(rnn_model_path)
 app= Flask(__name__)
 
@@ -49,8 +49,8 @@ def predict():
     prediction4= log_lin_reg_model.predict(final_features2)
     prediction5= log_svm_model.predict(final_features2)
     # prediction6= models['2 Log Random Forest'].predict(final_features2)
-    # prediction7= fnn_model.predict(final_features3)
-    # prediction8= cnn_model.predict(final_features3)
+    prediction7= fnn_model.predict(final_features3)
+    prediction8= cnn_model.predict(final_features3)
     # prediction9= rnn_model.predict(final_features3)
     
     output1= round(prediction1[0], 2)
@@ -59,8 +59,8 @@ def predict():
     output4= round(prediction4[0], 2)
     output5= round(prediction5[0], 2)
     # output6= round(prediction6[0], 2)
-    # output7= round(prediction7[0][0], 2)
-    # output8= round(prediction8[0][0], 2)
+    output7= round(prediction7[0][0], 2)
+    output8= round(prediction8[0][0], 2)
     # output9= round(prediction9[0][0], 2)
     
     return render_template('index.html', prediction_text1= output1,
@@ -69,8 +69,8 @@ def predict():
                            prediction_text4= output4,
                            prediction_text5= output5,
                            prediction_text6= 0,
-                           prediction_text7= 0,
-                           prediction_text8= 0,
+                           prediction_text7= output7,
+                           prediction_text8= output8,
                            prediction_text9= 0)
 
 @app.route('/predict_api', methods=['POST'])
